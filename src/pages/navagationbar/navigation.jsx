2b445db8dc3de7324928d22780c099b306d79f87
Julie Bissø <U2507981@rts-365.dk>
2025-06-13 12:27:29 +0200

    layout of html base made

---
+++ src/pages/navagationbar/navigation.jsx
@@ -0,0 +1,24 @@
+
+
+function Navegation() {
+
+  return (
+    <>
+<nav className="menu">
+  <ul>
+    <img src={reactLogo} alt="logo" />
+    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+
+<p>search bar here</p>
+
+    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+  </ul>
+</nav>
+    </>
+  )
+}
+
+export default navigation
