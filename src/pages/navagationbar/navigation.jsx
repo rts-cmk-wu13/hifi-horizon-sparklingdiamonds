166b5f70ae69da81a390aafa1af7a6f1051716f3
Julie Bissø <U2507981@rts-365.dk>
2025-06-13 13:03:50 +0200

    the basics of navigation done only missing dropdown and the real logo

--- src/pages/navagationbar/navigation.jsx
+++ src/pages/navagationbar/navigation.jsx
@@ -7,14 +7,14 @@ function Navegation() {
 <nav className="menu">
   <ul>
     <img src={reactLogo} alt="logo" />
-    <li className="menuList"><a className="menuLink" href="">icon</a></li>
-    <li className="menuList"><a className="menuLink" href="">icon</a></li>
-    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="#">shop</a></li>
+    <li className="menuList"><a className="menuLink" href="#">about us</a></li>
+    <li className="menuList"><a className="menuLink" href="#">contact us</a></li>
 
 <p>search bar here</p>
 
-    <li className="menuList"><a className="menuLink" href="">icon</a></li>
-    <li className="menuList"><a className="menuLink" href="">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="#">icon</a></li>
+    <li className="menuList"><a className="menuLink" href="#">icon</a></li>
   </ul>
 </nav>
     </>
