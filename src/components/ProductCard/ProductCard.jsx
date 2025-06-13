9788db51af9db4f362714f34e278355d355577c7
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-06-13 10:58:12 +0200

    ProductCard component med styling tilføjet

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ProductCards.scss';
+
+const ProductCards = ({ products }) => {
+  return (
+    <div className="productcards">
+      {products.map(product => (
+        <div key={product.id} className="productcards__card">
+          <img src={product.image} alt={product.title} />
+          <h4>{product.title}</h4>
+          <p>£ {product.price}</p>
+          <p><strong>Brand:</strong> {product.brand}</p>
+          <p><strong>Color:</strong> {product.color}</p>
+          <button>Add to cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductCards;
