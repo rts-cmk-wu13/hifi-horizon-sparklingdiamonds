3825df0112c4c16b899c1c6bec5c6cd0c7212086
Julie Bissø <U2507981@rts-365.dk>
2025-06-17 10:31:51 +0200

    did some styling on the hero image and on the what we do/opening hours

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,80 +3,81 @@ import "./HomePage.scss";
 export default function Home() {
 
     return (
-    <>
-  
-        
-        <article className="home">
+        <>
 
 
-        <img className="heroImgHome" src='/hero.svg' alt="to be hero_img" /> 
+            <article className="home">
 
-<section>
-    <h2>POPULAR PRODUCTS</h2>
-    <button></button>
-    <div>
-        {/* add popular products here */}
-    </div>
-</section>
-{/* end of popular products section */}
+                <div className="heroImgHome">
 
-<section className="OpeningTimeBox">
-<div>
-<h3>What we do</h3>
-<p>We look forward to customising a system to meet your needs.</p>
-<p>We don’t favour one manufacturer over another 
- the only thing we do favour is making sure our customers get the right product that suits their needs 
- and listening preferences. We will ask many questions in order to ensure that what you buy from us is tailored to you and you alone.</p>
-<p>If you are looking for a product not found in our demonstration showrooms or our online site, don’t fret as we have access to hundreds of brands.</p>
-<p>
-One of our biggest pleasures of working in this industry is to see the smile on our customers’ faces when they finally hear and see the system of their dreams.</p>
-</div>
+                    <img src='/hero.svg' alt="to be hero_img" />
+                </div>
+                <section>
+                    <h2>POPULAR PRODUCTS</h2>
+                    <button></button>
+                    <div>
+                        {/* add popular products here */}
+                    </div>
+                </section>
+                {/* end of popular products section */}
 
-<div>
-    <div>
-<h3>Opening hours</h3>
-<h4>Edinburgh</h4>
-<p>2 Joppa Rd,Edinburgh, EH15 2EU</p>
-<p>Monday to Friday: 10:00am - 5:30pm</p>
-<p>Saturday: 10:00am - 5:30pm</p>
-<p>Sunday: Closed</p>
-</div>
+                <section className="OpeningTimeBox">
+                    <div className="whatWeDo">
+                        <h2>What we do</h2>
+                        <p>We look forward to customising a system to meet your needs.</p>
+                        <p>We don’t favour one manufacturer over another
+                            the only thing we do favour is making sure our customers get the right product that suits their needs
+                            and listening preferences. We will ask many questions in order to ensure that what you buy from us is tailored to you and you alone.</p>
+                        <p>If you are looking for a product not found in our demonstration showrooms or our online site, don’t fret as we have access to hundreds of brands.</p>
+                        <p>
+                            One of our biggest pleasures of working in this industry is to see the smile on our customers’ faces when they finally hear and see the system of their dreams.</p>
+                    </div>
 
-<div>
-<h4>Falkirk</h4>
-<p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
-<p>Monday to Friday: 10:00am - 5:30pm</p>
-<p>Saturday - By appointment only</p>
-<p>Sunday: Closed</p>
-</div>
+                    <div className="openHours">
+                        <div>
+                            <h2>Opening hours</h2>
+                            <h4>Edinburgh</h4>
+                            <p>2 Joppa Rd,Edinburgh, EH15 2EU</p>
+                            <p>Monday to Friday: 10:00am - 5:30pm</p>
+                            <p>Saturday: 10:00am - 5:30pm</p>
+                            <p>Sunday: Closed</p>
+                        </div>
 
-</div>{/* end of opening hours */}
-</section>
-{/* end of what we do and opening hours sectio */}
+                        <div>
+                            <h4>Falkirk</h4>
+                            <p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
+                            <p>Monday to Friday: 10:00am - 5:30pm</p>
+                            <p>Saturday - By appointment only</p>
+                            <p>Sunday: Closed</p>
+                        </div>
+
+                    </div>{/* end of opening hours */}
+                </section>
+                {/* end of what we do and opening hours sectio */}
 
 
-<section>
+                <section>
 
-    <div>
-        <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-         <p>this is not perm</p>
-        
-    </div>
-    {/* sign up section */}
-</section>
+                    <div>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
+                        <p>this is not perm</p>
 
-        </article>
-    </>
-       
-        
+                    </div>
+                    {/* sign up section */}
+                </section>
+
+            </article>
+        </>
+
+
     )
 }
