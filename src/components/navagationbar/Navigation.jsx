bc22b835576d4c2f3b9f36ed18509317e5d3b2dd
Denisse Daugaard <U2478723@rts-365.dk>
2025-06-20 09:37:44 +0200

    drop and search working fine !

--- src/components/navagationbar/Navigation.jsx
+++ src/components/navagationbar/Navigation.jsx
@@ -1,12 +1,14 @@
 import './navStyle.scss'
 
 import '../SearchBar/SearchBar.scss'
-import Drop from './dropdown';
+import Drop from './dropdown.jsx'
 
 import { NavLink } from 'react-router'
 import { FaUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import LogoutButton from '../LogoutButton'
+import SearchBar from '../SearchBar/SearchBar'
+
 
 
 
@@ -23,16 +25,12 @@ export default function Navegation() {
 <Drop />
 
       
-          <li className="menuList"><NavLink className="menuLink" to="about">ABOUT US</NavLink></li>
-          <li className="menuList"><NavLink className="menuLink" to="contact">CONTACT US</NavLink></li>
-          <li className="menuList search-bar">
-            <div className="search-container">
-              <input type="search" placeholder="Search product..." />
-              <button><FaSearch /></button>
-            </div>
-          </li>
-          <li className="menuList"><NavLink className="menuLink" to="profile"><FaUser /></NavLink></li>
-          <li className="menuList"> <NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink></li>
+          
+    <li className="menuList"><NavLink className="menuLink" to="about">ABOUT US</NavLink></li>
+    <li className="menuList"><NavLink className="menuLink" to="contact">CONTACT US</NavLink></li>
+    <li className="menuList search-bar"><SearchBar/></li>
+    <li className="menuList"><NavLink className="menuLink" to="profile"><FaUser /></NavLink></li>
+    <li className="menuList"> <NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink></li>
 
           {location.pathname !== '/login' && (
             <>
