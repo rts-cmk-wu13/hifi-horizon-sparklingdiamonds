4cc983804a228eb57f043cf47c979131980fdf70
Julie Bissø <U2507981@rts-365.dk>
2025-06-24 13:12:57 +0200

    started working on burger menu

--- src/components/navagationbar/Navigation.jsx
+++ src/components/navagationbar/Navigation.jsx
@@ -3,6 +3,7 @@ import './navStyle.scss'
 import '../SearchBar/SearchBar.scss'
 import Drop from './dropdown.jsx'
 
+
 import { NavLink } from 'react-router'
 import { FaUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
@@ -21,15 +22,12 @@ export default function Navegation() {
         <ul>
           <NavLink to="/"><img src='/logo.svg' alt="logo" /></NavLink>
 
-<Drop />
-
-      
-          
-    <li className="menuList"><NavLink className="menuLink" to="about">ABOUT US</NavLink></li>
-    <li className="menuList"><NavLink className="menuLink" to="contact">CONTACT US</NavLink></li>
-    <li className="menuList search-bar"><SearchBar/></li>
-    <li className="menuList"><NavLink className="menuLink" to="profile"><FaUser /></NavLink></li>
-    <li className="menuList"> <NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink></li>
+          <Drop />
+          <li className="menuList"><NavLink className="menuLink" to="about">ABOUT US</NavLink></li>
+          <li className="menuList"><NavLink className="menuLink" to="contact">CONTACT US</NavLink></li>
+          <li className="menuList search-bar"><SearchBar /></li>
+          <li className="menuList"><NavLink className="menuLink" to="profile"><FaUser /></NavLink></li>
+          <li className="menuList"> <NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink></li>
 
           {location.pathname !== '/login' && (
             <>
@@ -41,5 +39,47 @@ export default function Navegation() {
         </ul>
       </nav>
     </>
+
+
   )
+} 
+
+/* import './navStyle.scss';
+import '../SearchBar/SearchBar.scss';
+import Drop from './dropdown.jsx';
+
+import { NavLink, useLocation } from 'react-router-dom'; // FIXED: react-router → react-router-dom
+import { FaUser, FaShoppingCart, FaBars } from "react-icons/fa";
+import LogoutButton from '../LogoutButton';
+import SearchBar from '../SearchBar/SearchBar';
+import { useState } from 'react';
+
+export default function Navegation() {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation(); // FIXED: Needed for pathname
+
+  return (
+    <nav className={`menu ${isOpen ? 'open' : ''}`}>
+      <div className="nav-header">
+
+      
+        <div className="burger" onClick={() => setIsOpen(!isOpen)}>
+          <FaBars />
+        </div>
+      </div>
+
+      <ul className="menu-links">
+
+        <NavLink to="/"><img src="/logo.svg" alt="logo" /></NavLink>
+        <Drop />
+        <li className="menuList"><NavLink className="menuLink" to="about">ABOUT US</NavLink></li>
+        <li className="menuList"><NavLink className="menuLink" to="contact">CONTACT US</NavLink></li>
+        <li className="menuList search-bar"><SearchBar /></li>
+        <li className="menuList"><NavLink className="menuLink" to="profile"><FaUser /></NavLink></li>
+        <li className="menuList"><NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink></li>
+        {location.pathname !== '/login' && <LogoutButton />}
+      </ul>
+    </nav>
+  );
 }
+ */
