2c314f68d0467a4812f6681c204a61f8ce044da7
Julie Bissø <U2507981@rts-365.dk>
2025-06-25 12:18:00 +0200

    done the burger as well as i could, it works but stylling is a bit off some places

--- src/components/navagationbar/burger.jsx
+++ src/components/navagationbar/burger.jsx
@@ -5,46 +5,46 @@ import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
 import SearchBar from '../SearchBar/SearchBar'
 
-import { FaUser,  FaShoppingCart  } from "react-icons/fa";
+import { FaUser, FaShoppingCart } from "react-icons/fa";
 
 
 export default function BottomBurgerMenu() {
     const [isOpen, setIsOpen] = useState(false);
-  const [showSearch, setShowSearch] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
 
 
 
     return (
         <div className="bottom-menu-container">
 
-                {showSearch && (
-        <SearchBar />
-      )}
+            {showSearch && (
+                <SearchBar />
+            )}
             <div className='bottomMenuBar'>
-                       <div className={`menu-drawer ${isOpen ? "open" : ""}`}>
-                <NavLink to="/"><img src='/logo.svg' alt="logo" /></NavLink>
-              <NavLink className="menuLink" to="products">SHOP</NavLink>
-              <NavLink className="menuLink" to="about">ABOUT US</NavLink>
-               <NavLink className="menuLink" to="contact">CONTACT US</NavLink>
+                <div className={`menu-drawer ${isOpen ? "open" : ""}`}>
+                    <NavLink to="/"><img src='/logo.svg' alt="logo" /></NavLink>
+                    <NavLink className="menuLink" to="products">SHOP</NavLink>
+                    <NavLink className="menuLink" to="about">ABOUT US</NavLink>
+                    <NavLink className="menuLink" to="contact">CONTACT US</NavLink>
 
-            </div>
-              <button className="search-icon" onClick={() => setShowSearch(!showSearch)}>
-          <FaSearch />
-        </button>
+                </div>
+                <button className="search-icon" onClick={() => setShowSearch(!showSearch)}>
+                    <FaSearch />
+                </button>
 
                 <button className='ToprofileBTN'>
-<NavLink className="menuLink" to="profile"><FaUser /></NavLink>
+                    <NavLink className="menuLink" to="profile"><FaUser /></NavLink>
                 </button>
-                <button  className='ToCartBTN'>
-<NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink>
+                <button className='ToCartBTN'>
+                    <NavLink className="menuLink" to="cart"><FaShoppingCart /></NavLink>
                 </button>
-                
-            <button
-                className="burger-toggle-menu" onClick={() => setIsOpen((prev) => !prev)}>
-                {isOpen ? <FaTimes /> : <FaBars />}
-            </button>
-            
-       </div>
+
+                <button
+                    className="burger-toggle-menu" onClick={() => setIsOpen((prev) => !prev)}>
+                    {isOpen ? <FaTimes /> : <FaBars />}
+                </button>
+
+            </div>
 
         </div>
     );
