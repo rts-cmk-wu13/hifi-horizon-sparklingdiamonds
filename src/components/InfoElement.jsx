d704decdf7108a2b98df20820f16623f81e7104a
Denisse Daugaard <U2478723@rts-365.dk>
2025-06-25 14:09:39 +0200

    Cleaned up InfoElement component to resolve Netlify bundling issue

--- src/components/InfoElement.jsx
+++ src/components/InfoElement.jsx
@@ -1,8 +1,8 @@
-
+import React from 'react';
  "../pages/Profile/EditProfile";
 
 
-export default function InfoElement({icon,child,elementName,elementInfo,addressChild}) {
+export default function InfoElement({icon,elementName,elementInfo,addressChild}) {
 
     return (
         <>
